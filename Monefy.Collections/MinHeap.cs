1396c78c234a8e801e0c99f7884b8e5d7b3ed01f
yblinov <yurii.blinov@gmail.com>
2016-08-27 22:54:41 +0300

    Initial commit

---
+++ Monefy.Collections/MinHeap.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Monefy.Collections
+{
+    public class MinHeap<T>
+    {
+        public MinHeap()
+        {
+        }
+    }
+}
